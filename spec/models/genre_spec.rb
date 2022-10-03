71545a3beaf0b9e0cc1b7a4c1833923c2007d19a
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-10-03 14:16:30 -0300

    genre tests

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Testing factory" do
+    it { expect(build(:genre)).to be_valid }
+  end
+
+  context "Validating name" do
+    it " should be invalid if nil" do
+      expect(build(:genre, name: nil)).to be_invalid
+    end
+
+    it " should be invalid if repeated" do
+      create(:genre, name: "Terror")
+      expect(build(:genre, name: "Terror")).to be_invalid
+    end
+  end
 end
