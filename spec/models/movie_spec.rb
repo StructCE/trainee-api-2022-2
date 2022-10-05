180d440c39af073d6e6388f95d0ef63fd8bd6bbb
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-10-05 12:14:21 -0300

    movie tests

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Testing factory" do
+    it { expect(build(:movie)).to be_valid }
+  end
+
+  context "Validating name" do
+    it " should be invalid if nil" do
+      expect(build(:movie, name: nil)).to be_invalid
+    end
+
+    it " should be invalid if repeated" do
+      create(:movie, name: "Deadpool")
+      expect(build(:movie, name: "Deadpool")).to be_invalid
+    end
+  end
+
+  context "Validating genre" do
+    it " should be invalid if nil" do
+      expect(build(:movie, genre: nil)).to be_invalid
+    end
+
+    it " should be invalid if doesn't exist" do
+      create(:movie, name: "Deadpool")
+      expect(build(:movie, name: "Deadpool")).to be_invalid
+    end
+  end
 end
