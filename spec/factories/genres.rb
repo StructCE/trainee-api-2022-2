fdf4f1a6314a74c95481423e36a3d2410ad7f75f
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-10-05 12:14:11 -0300

    genre, movies, users factories updated

--- spec/factories/genres.rb
+++ spec/factories/genres.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
+  # Nome de filme deve ser único. Devemos garantir que chama
+  # create(:movie) duas vezes não irá dar problemas por usar o
+  # mesmo nome, então criamos uma sequencia para que o nome nunca
+  # se repita, a nao ser que propositalmente
+  sequence :genre_names do |n|
+    "Genero #{n}"
+  end
+
   factory :genre do
-    name { "MyString" }
+    # generate chama a sequência.
+    name { generate(:genre_names) }
   end
 end
