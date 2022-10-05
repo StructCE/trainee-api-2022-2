924bbdbb5616f05363c5adf9b7ea094992354a59
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-10-05 12:26:55 -0300

    rating test #1

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Testing factory" do
+    it { expect(build(:rating)).to be_valid }
+  end
+
+  context "Validating comment" do
+    it " should be invalid if empty" do
+      expect(build(:rating, comment: "")).to be_invalid
+    end
+  end
+
 end
