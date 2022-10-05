67a80535f3a0267ba6023275aa9ffa0bb4a492ac
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-10-05 12:26:40 -0300

    factory ratings updated

--- spec/factories/ratings.rb
+++ spec/factories/ratings.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :rating do
     grade { false }
     comment { "MyString" }
-    user { nil }
-    movie { nil }
+    user { association :user }
+    movie { association :movie }
   end
 end
